0d5e6f05fd86b138a54cbea005480223e25e42c6
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-02-26 17:33:20 -0800

    thinking in components

--- sfpopos/src/App.js
+++ sfpopos/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Title from './Title.js';
 import POPOSList from './POPOSList';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <POPOSList />
+      <Footer />
     </div>
   );
 }
---
+++ sfpopos/src/Footer.js
@@ -0,0 +1,15 @@
+// src/Title.js
+
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h1>Danika Jaskulak-Gonzalez</h1>
+            <div className="Footer-Subtitle">Make School Term 3, 2021</div>
+    </div>
+  )
+}
+
+export default Footer
